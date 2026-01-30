ecdbcde504cf44f7bd2570c963ade142bffd6219
Felix Bay <fe.bay@icloud.com>
2026-01-30 16:40:46 +0100

    remove schedule and add debounce on rotary_encoder

--- src/rotary_encoder.py
+++ src/rotary_encoder.py
@@ -1,11 +1,17 @@
-from asyncio import Event
+from asyncio import Event, ThreadSafeFlag, create_task, sleep_ms
 from machine import Pin 
-from micropython import schedule
+from micropython import const
+import time
 
+ROTARY_DEBOUNCE = const(50) # milliseconds
+BUTTON_DEBOUNCE = const(50) # milliseconds
 
 class RotaryEncoder:
 	rotary_event: Event
 	button_event: Event
+
+	_rotary_flag: ThreadSafeFlag
+	_button_flag: ThreadSafeFlag
 
 	_delta: int
 	_clk: Pin
@@ -16,6 +22,9 @@ class RotaryEncoder:
 		self.rotary_event = Event()
 		self.button_event = Event()
 
+		self._rotary_flag = ThreadSafeFlag()
+		self._button_flag = ThreadSafeFlag()
+
 		self._delta = 0
 
 		self._clk = Pin(clk, Pin.IN, Pin.PULL_UP)
@@ -25,6 +34,10 @@ class RotaryEncoder:
 		self._clk.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING,
 					 handler=self._rotary_irq_callback)
 		self._sw.irq(trigger=Pin.IRQ_FALLING, handler=self._button_irq_callback)
+
+
+		create_task(self._rotary_flag_listener())
+		create_task(self._button_flag_listener())
 		pass
 
 	def drain_delta(self) -> int:
@@ -34,8 +47,19 @@ class RotaryEncoder:
 
 	def _rotary_irq_callback(self, _) -> None:
 		self._delta += 1 if self._clk.value() != self._dt.value() else -1
-		schedule(lambda _: self.rotary_event.set(), None)
+		self._rotary_flag.set()
 
 	def _button_irq_callback(self, _) -> None:
-		schedule(lambda _: self.button_event.set(), None)
+		self._button_flag.set()
 	
+	async def _rotary_flag_listener(self) -> None:
+		while True:
+			await self._rotary_flag.wait()
+			await sleep_ms(ROTARY_DEBOUNCE)
+			self.rotary_event.set()
+
+	async def _button_flag_listener(self) -> None:
+		while True:
+			await self._button_flag.wait()
+			await sleep_ms(BUTTON_DEBOUNCE)
+			self.button_event.set()
